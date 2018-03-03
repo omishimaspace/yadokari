de5df840c367666e0b5ea9a8179516337e3536ef
Shigeki Masuda <macha3162@gmail.com>
2018-03-03 16:27:14 +0900

    フォルダ名をミスったのを修正.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :reservations
+      resources :rooms
+      resources :yados
+    end
+  end
+
   resources :reservations
   resources :rooms
   resources :yados
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
