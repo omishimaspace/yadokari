4fb1d27b173b5f3c25a21daa151e47b4dabbdfac
Shigeki Masuda <macha3162@gmail.com>
2018-03-12 14:31:52 +0900

    返却する値を制限.

--- app/controllers/api/v1/yados_controller.rb
+++ app/controllers/api/v1/yados_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::YadosController < Api::BaseController
   def index
     @yados = Yado.all.order(:id)
-    render json: @yados
+    render json: @yados.map{|y|y.as_json(except: %i(url note created_at updated_at))}
   end
 
   def show
     @yado = Yado.friendly.find(params[:id])
-    render json: @yado
+    render json: @yado.as_json(except: %i(created_at updated_at))
   end
 end
