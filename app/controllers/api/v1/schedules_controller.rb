f6d2dd6d31a3824606a6876bc53d1fa4396143e3
Shigeki Masuda <masuda@c-fo.com>
2018-08-24 23:12:12 +0900

    カレンダー参照機能追加

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::SchedulesController < Api::BaseController
 
+  require 'open-uri'
   def index
-    schedules = Schedule.future.order(:started_on)
-    render json: schedules.map{|s|s.to_cal}
+    schedules = []
+    if ENV['ICAL_URL'].present?
+      ics = open(ENV['ICAL_URL'])
+      cals = Icalendar::Calendar.parse(ics)
+      ics.close
+
+      today = Date.today
+      schedules = cals.first.events.sort { |a, b| a.dtstart <=> b.dtstart }.select{|d| d.dtstart >= today }.map do |event|
+        "#{event.dtstart.strftime("%F")} #{event.summary}"
+      end
+    end
+    render json: schedules
   end
 end
